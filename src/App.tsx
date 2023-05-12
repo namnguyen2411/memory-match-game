35afee3f3bbd974a407df9cbbb9f871ea1548c39
NamNP-PC <phuongnam.nguyen241196@gmail.com>
2023-05-13 00:44:49 +0700

    setup base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold text-cyan-500">
+        Memory Card Game - Version Pokemon
+      </h1>
+    </div>
+  );
+};
+
+export default App;
